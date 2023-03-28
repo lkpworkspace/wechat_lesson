f9282d21ea6f3381f3042a7bb267015c8e8cec60
likepeng <likepeng0418@163.com>
2023-03-28 17:25:34 +0800

    lesson2

--- wechat_lesson.cpp
+++ wechat_lesson.cpp
@@ -28,10 +28,83 @@ class wechat_lessonActor : public myframe::Actor {
       http_req = msg->GetAnyData<std::shared_ptr<myframe::HttpReq>>();
     } catch (std::exception& e) {
       LOG(ERROR) << "get http req failed";
+      ReplyHttp(msg->GetSrc(), 404, "");
       return;
     }
     // 打印http请求
     LOG(INFO) << *msg << " " << http_req->DebugString();
+    // 解析uri中的参数
+    Json::Value resource;
+    if (!ParseResourceJson(http_req->uri(), &resource)) {
+      LOG(ERROR) << "parse resource failed";
+      ReplyHttp(msg->GetSrc(), 404, "");
+      return;
+    }
+    // 判断是否微信服务器验证消息
+    if (resource.isMember("params") && resource["params"].isMember("echostr")) {
+      ReplyHttp(msg->GetSrc(), 200, resource["params"]["echostr"].asString());
+      return;
+    }
+    // 回显请求
+    ReplyHttp(msg->GetSrc(), 200, http_req->DebugString());
+  }
+
+  void ReplyHttp(const std::string& dst, int code, const std::string& body) {
+    auto resp = std::make_shared<myframe::HttpResp>();
+    resp->set_code(code);
+    resp->set_body(body);
+    LOG(INFO)
+      << "resp http msg code "
+      << resp->code()
+      << (resp->body().size() > 512 ? "" : (", body: " + resp->body()));
+    auto msg = std::make_shared<myframe::Msg>();
+    msg->SetAnyData(resp);
+    auto mailbox = GetMailbox();
+    mailbox->Send(dst, msg);
+  }
+
+  std::vector<std::string> Split(const std::string& str, char delim) {
+    std::vector<std::string> name_list;
+    std::string item;
+    std::stringstream ss(str);
+    while (std::getline(ss, item, delim)) {
+      name_list.push_back(item);
+    }
+    return name_list;
+  }
+
+  /*
+  将resource解析成如下格式的json数据结构
+  {
+    "path":"",
+    "params":{
+      "":"",
+      "",""
+    }
+  }
+  */
+  bool ParseResourceJson(const std::string& s, Json::Value* jv_ptr) {
+    auto& v = *jv_ptr;
+    auto pos = s.find('?');
+    if (pos == s.npos) {
+      // TODO check path is ok
+      v["path"] = s;
+      return true;
+    }
+    // TODO check path is ok
+    v["path"] = s.substr(0, pos);
+    auto params_str = s.substr(pos + 1);
+    // spilt params
+    auto param_pair = Split(params_str, '&');
+    for (int i = 0; i < param_pair.size(); ++i) {
+      auto pair = Split(param_pair[i], '=');
+      if (pair.size() != 2) {
+        LOG(ERROR) << "param fmt error";
+        return false;
+      }
+      v["params"][pair[0]] = pair[1];
+    }
+    return true;
   }
 };
 
